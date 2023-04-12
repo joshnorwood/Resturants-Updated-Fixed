4b811b321b155279d97ae32dcd1c20061a0f3818
joshnorwood <joshnorwood03@gmail.com>
2023-04-12 02:06:58 -0400

    New Aprl 12 2 AM

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Resturants_Updated_Fixed.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
+
